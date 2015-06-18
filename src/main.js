375109f7269e71d5d8c3de1d8aec8b138bba4080
treelite <c.xinle@gmail.com>
2015-06-18 23:08:03 +0800

    Support path params

--- src/main.js
+++ src/main.js
@@ -11,6 +11,9 @@ define(function (require) {
     var curry = require('saber-lang/curry');
     var extend = require('saber-lang/extend');
 
+    var router = require('saber-router');
+    router.controller(require('saber-router/controller/page'));
+
     var exports = {};
 
     Emitter.mixin(exports);
@@ -66,22 +69,16 @@ define(function (require) {
     }
 
     /**
-     * 引导Presenter
+     * 启动Presenter
      *
      * @inner
+     * @param {string} path 请求路径
+     * @param {Object} query 查询条件
      * @param {Object} options 跳转参数
+     * @param {Function} done 路由处理完成回调
      * @param {Presenter} presenter Presenter对象
      */
-    function boot(options, presenter) {
-        // TODO
-        // Parse path params
-        var parseQuery = require('saber-uri/util/parse-query');
-        var path = location.pathname;
-        var query = parseQuery(location.search.substring(1));
-        var url = path;
-        if (location.search.length > 1) {
-            url += location.search;
-        }
+    function boot(path, query, options, done, presenter) {
 
         function success() {
             window.addEventListener(
@@ -91,25 +88,28 @@ define(function (require) {
             );
             presenter.ready();
             presenter.complete();
+            done();
         }
 
         function fail(reason) {
             exports.emit('error', reason);
+            done();
         }
 
         // 判断是否应该进行首屏渲染
         var needRender = !config.isomorphic && config.renderFirst;
 
+        var main = config.main || document.body;
         // 启动首渲染
         if (needRender) {
             presenter
-                .enter(document.body, path, query, url, options)
+                .enter(main, path, query, options)
                 .then(success, fail);
         }
         else {
             // 不再渲染首屏
             presenter.set(path);
-            presenter.view.set(document.body);
+            presenter.view.set(main);
             if (config.isomorphic) {
                 presenter.model.fill(getSyncData('model'));
             }
@@ -118,10 +118,27 @@ define(function (require) {
     }
 
     /**
+     * 路由处理
+     *
+     * @inner
+     * @param {Object|string} presenter presenter 配置信息或者配置信息加载地址
+     * @param {Object} options 附加参数
+     * @return {Function}
+     */
+    function routeTo(presenter, options) {
+        return function (path, query, params, url, opts, done) {
+            options = extend(opts, options);
+            query = extend(params, query);
+            mm.create(presenter).then(curry(boot, path, query, options, done));
+        };
+    }
+
+    /**
      * 配置
      *
      * @public
      * @param {Object} options 配置项
+     * @param {HTMLElement} options.main 主元素
      * @param {boolean=} options.renderFirst 是否启用首屏渲染，默认为启动
      * @param {string|Array.<string>=} options.template 通用模版
      * @param {Object=} options.templateConfig 模版引擎配置
@@ -138,20 +155,23 @@ define(function (require) {
      * 启动Presenter
      *
      * @public
-     * @param {Object} presenter Presenter配置参数
+     * @param {Object|string} presenter Presenter 配置信息或者配置信息的加载地址
+     * @param {string|RegExp=} path 路由路径
      * @param {Object=} options 附加参数
      */
-    exports.boot = function (presenter, options) {
+    exports.boot = function (presenter, path, options) {
         // 设置saber-mm
         var config4mm = getConfig4MM();
-        var router = require('saber-router');
-        router.controller(require('saber-router/controller/page'));
         config4mm.router = router;
         // 扩展同步的模版静态数据
         config4mm.templateData = extend({}, getSyncData('templateData'), config4mm.templateData);
         mm.config(config4mm);
 
-        mm.create(presenter).then(curry(boot, options));
+        // 启用路由器
+        path = path || location.pathname;
+        router.add(path, routeTo(presenter, options));
+
+        router.start();
     };
 
     /**
